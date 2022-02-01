4bced3051fa33ec682c617b24524b7d76d35ab1b
kstrick01 <91700084+kstrick01@users.noreply.github.com>
2022-01-31 20:17:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 function compute()
+{
     {
         var principal = document.getElementById("principal").value;
         var rate = document.getElementById("rate").value;
         var years = document.getElementById("years").value;
         var interest = principal * years * rate /100;
         var year= new Date(year).getFullYear+parseInt(years);
-                if(principal.value <= 0){
+        if(principal.value <= 0){
             alert("Please enter a positive number");
             document.getElementById("principal").focus(); 
              }else{
                 alert("success");
              }
-       var results = document.getElementById("result").innerHTML="If you deposit"+ <mark>principal</mark>+",\<br\>at an interest rate of "+ <mark>rate+ </mark>"%\<br\>You will receive an amount of "+ <mark>interest" </mark>,\<br\> in the year "+ <mark>year <mark>+"\<br\>";
-         
-    }
-     
-  function updateRate(){
-            var rateval = document.getElementById("rate").value;
-               document.getElementById("rate_val").innerText=rateval;
+         }
+
+       var results = document.getElementById("result").innerHTML="If you deposit "+<mark>principal</mark>+",\<br\>at an interest rate of "+<mark>rate</mark>+"%\<br\>You will receive an amount of "+<mark>interest</mark>+",\<br\>in the year "+<mark>year</mark>+"\<br\>"
+    
+}   function updateRate(){
+ 
+         var rateval =  document.getElementById("rate").value;
+         document.getElementById("rate_val").innerText=rateval;
+
             
          }
