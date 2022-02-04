05b95a45a042970f1535b5ed516dc3286734d827
kstrick01 <91700084+kstrick01@users.noreply.github.com>
2022-02-04 00:09:57 -0500

    Final changes

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-function compute()
-{
-    
-        var principal = document.getElementById("principal");
-        var rate = document.getElementById("rate").value;
-        var years = document.getElementById("years").value;
-        var interest = principal * years * rate /100;
-        var year= new Date(year).getFullYear+parseInt(years);
-        var results = document.getElementById("result");
-        if(principal.value <= 0){
-            alert("Please enter a positive number");
-            document.getElementById("principal").focus(); 
-             }else {
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var result = document.getElementById("result");
+
+    //Check for 0 & negative number input
+    if (principal <= "0") {
+        alert('Please enter a positive number!');
+        document.getElementById("principal").focus();
+
+    // Display the result if input is a positive integer    
+    } else {
         result.innerHTML = '\<br\>' + 'If you deposit ' + '<mark>' + principal + '</mark>' + ',' + '\<br\>'
             + 'at an interest rate of ' + '<mark>' + rate + '%' + '</mark>' + ',' + '\<br\>'
             + 'You will receive an amount of ' + '<mark>' + interest + '</mark>' + ',' + '\<br\>'
             + 'in the year ' + '<mark>' + year + '</mark>'
-             }
-        
+    }
+}
 
-       var results = document.getElementById("result")
-}   function updateRate(){
- 
-         var rateval =  document.getElementById("rate").value;
-        document.getElementById("rate_val").innerText = rateval + '%';
 
-            
-         }
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + '%';
+}
